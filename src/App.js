7dcfc9bed0cac3e95229f99950b55815ea4e0b52
Won Young Choi <wc7qpe@virginia.edu>
2020-10-02 08:56:20 -0400

    finished the dice roll app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+function App(){
+
+  const [diceImage, roll] = useState(null);
+  const [diceNum, rollNum] = useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center"}}>
+      <img src={diceImage} style={{ width: "25%" }}></img> <br></br>
+      <h4>{diceNum}</h4>
+      <button onClick={ () => {
+          let rand = Math.floor(Math.random() * (7 - 1) + 1);
+          switch(rand){
+            case 1:
+              roll("https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Dice-1-b.svg/1024px-Dice-1-b.svg.png");
+              rollNum("You rolled a " + rand + "!");
+              break;
+            case 2: 
+              roll("https://www.iconfinder.com/data/icons/dice-roll/100/dice_2-512.png");
+              rollNum("You rolled a " + rand + "!");
+              break;
+            case 3: 
+              roll("https://www.iconfinder.com/data/icons/dice-roll/100/dice_3-512.png");
+              rollNum("You rolled a " + rand + "!");
+              break;
+            case 4:
+              roll("https://www.iconfinder.com/data/icons/dice-roll/100/dice_4-512.png");
+              rollNum("You rolled a " + rand + "!");
+              break;
+            case 5:
+              roll("https://www.iconfinder.com/data/icons/dice-roll/100/dice_5-512.png");
+              rollNum("You rolled a " + rand + "!");
+              break;
+            default:
+              roll("https://www.iconfinder.com/data/icons/dice-roll/100/dice_6-512.png");
+              rollNum("You rolled a " + rand + "!");
+          }
+        }
+      }>Roll Dice
+      </button> <br/>
     </div>
   );
 }
+
 
 export default App;
