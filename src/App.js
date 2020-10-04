331c168d6c0a0194b484c13b044e94d80a299705
Won Young Choi <wc7qpe@virginia.edu>
2020-10-04 14:58:00 -0400

    Updated the application to make it more efficient and clean

--- src/App.js
+++ src/App.js
@@ -1,45 +1,29 @@
 import React, {useState} from 'react';
 import './App.css';
+import side1 from "./images/side1.png";
+import side2 from "./images/side2.png";
+import side3 from "./images/side3.png";
+import side4 from "./images/side4.png";
+import side5 from "./images/side5.png";
+import side6 from "./images/side6.png";
 
 function App(){
 
-  const [diceImage, roll] = useState(null);
-  const [diceNum, rollNum] = useState(null)
+  const [num, roll] = useState(null);
+  let images = [side1, side2, side3, side4, side5, side6];
+
+
+  const rollDice = () => {
+    let rand = Math.floor(Math.random() * 6 + 1);
+    roll(rand);
+  }
 
   return (
     <div style={{ textAlign: "center"}}>
-      <img src={diceImage} style={{ width: "25%" }}></img> <br></br>
-      <h4>{diceNum}</h4>
-      <button onClick={ () => {
-          let rand = Math.floor(Math.random() * (7 - 1) + 1);
-          switch(rand){
-            case 1:
-              roll("https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Dice-1-b.svg/1024px-Dice-1-b.svg.png");
-              rollNum("You rolled a " + rand + "!");
-              break;
-            case 2: 
-              roll("https://www.iconfinder.com/data/icons/dice-roll/100/dice_2-512.png");
-              rollNum("You rolled a " + rand + "!");
-              break;
-            case 3: 
-              roll("https://www.iconfinder.com/data/icons/dice-roll/100/dice_3-512.png");
-              rollNum("You rolled a " + rand + "!");
-              break;
-            case 4:
-              roll("https://www.iconfinder.com/data/icons/dice-roll/100/dice_4-512.png");
-              rollNum("You rolled a " + rand + "!");
-              break;
-            case 5:
-              roll("https://www.iconfinder.com/data/icons/dice-roll/100/dice_5-512.png");
-              rollNum("You rolled a " + rand + "!");
-              break;
-            default:
-              roll("https://www.iconfinder.com/data/icons/dice-roll/100/dice_6-512.png");
-              rollNum("You rolled a " + rand + "!");
-          }
-        }
-      }>Roll Dice
-      </button> <br/>
+      <br></br>
+      <img src={images[num - 1]} style={{ width: "25%", height: "25%" }}></img> <br></br>
+      {num && <h4>You rolled a {num}</h4>}
+      <button onClick={rollDice}>Roll Dice</button> <br/>
     </div>
   );
 }
